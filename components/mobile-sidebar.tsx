f37c209865bff77aeaf192b0d8f8d142f23289d8
ShuhaiYu <1141071640@qq.com>
2024-07-11 17:27:42 +1000

    feat: Add AI-powered tools to the dashboard page

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+  
   return (
     <Sheet>
       <SheetTrigger>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboardIcon, MessageSquare, MusicIcon, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -54,6 +55,8 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -67,7 +70,7 @@ const Sidebar = () => {
         </Link>
         <div className="space-y-1">
             {routes.map((route) => (
-                <Link key={route.name} href={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+                <Link key={route.name} href={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
                     <div className="flex items-center flex-1">
                         <route.icon className={cn("w-5 h-5 mr-3", route.color)} />
                         <span className="text-sm">{route.name}</span>
